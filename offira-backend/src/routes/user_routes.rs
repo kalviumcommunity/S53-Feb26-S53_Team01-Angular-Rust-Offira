e432a2ec3df0b7dea26679cfddc73d739264ab18
Megha Wadhwa <meghawadhwa20@gmail.com>
2026-03-06 16:01:25 +0530

    feat: add GET /users/{id} endpoint

--- offira-backend/src/routes/user_routes.rs
+++ offira-backend/src/routes/user_routes.rs
@@ -2,9 +2,11 @@ use axum::{Router, routing::get};
 use sqlx::PgPool;
 
 use crate::handlers::user_handlers::get_users;
+use crate::handlers::user_handlers::get_user_by_id;
 
 pub fn user_routes(pool: PgPool) -> Router {
     Router::new()
         .route("/users", get(get_users))
+        .route("/users/{id}", get(get_user_by_id))
         .with_state(pool)
 }
